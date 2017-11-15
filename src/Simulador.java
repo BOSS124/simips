c0a74f3e4febfde8ccffa4e6f3a895a0a0c3b489
dallok <igortrevelin@usp.br>
2017-11-15 20:22:48 -0200

    Simulador class

---
+++ src/Simulador.java
@@ -0,0 +1,25 @@
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+
+public class Simulador extends JPanel implements Runnable {
+	public static final String frameTitle = "Simulador ";
+	private JFrame frame;
+
+	public Simulador() {
+		frame = new JFrame(frameTitle);
+
+		
+	}
+	
+	public void tick() {
+
+	}
+
+	public void render() {
+
+	}
+
+	public void run() {
+
+	}
+}
